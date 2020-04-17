fd96590879e7dba35e61968d29b1da483fd378c6
byron7cueva <byron7cueva@gmail.com>
2020-04-17 16:13:51 -0500

    Add default themes

---
+++ src/styles/defaults/global.js
@@ -0,0 +1,15 @@
+import { css } from 'styled-components'
+
+import { font, size } from '../../config/siteConstants'
+
+export const defaults = css`
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: ${font.content};
+    font-size: ${size.text};
+  }
+`
--- src/styles/main.js
+++ src/styles/main.js
@@ -1,7 +1,9 @@
 import{ createGlobalStyle } from 'styled-components'
 
 import { variables } from './defaults/variables'
+import { defaults } from './defaults/global'
 
 export const Style = createGlobalStyle`
   ${variables}
+  ${defaults}
 `
