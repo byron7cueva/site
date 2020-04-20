50786ec1b56df7f437effcbf49f6cb4fe87a8502
byron7cueva <byron7cueva@gmail.com>
2020-04-20 18:57:47 -0500

    Change size icon and color on opinion section

--- src/components/Opinion/index.js
+++ src/components/Opinion/index.js
@@ -29,7 +29,7 @@ export const Opinion = ({ message, link, writer, position, photo }) => {
         <p className='opinion__message'>
           <IoIosQuote size={size.icon} className='opinion__message__left-quote' color={`rgb(${color.rgb.dark})`}/>
           {message}
-          <IoIosQuote size={20} />
+          <IoIosQuote size={size.icon} color={`rgb(${color.rgb.dark})`} />
           <a href={link} target='noopener noreferrer'>{' '}Ver más..</a>
         </p>
         <div className='opinion__writer'>
